4d1aa4b4c689dea99cace6409f523f54037a0d33
Jose Rosales <jarc_lex@hotmail.com>
2020-02-06 15:15:59 -0600

    Updates

---
+++ precache-manifest.d71cf0307b0872b2e422e4d1f90f7576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e640b0cc6e11aa4e61995d406eb9d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0ce9968d19baafb82235",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b8d56dbd8c7b79859ea6",
+    "url": "/react-gh-pages/static/js/2.02494662.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.02494662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce9968d19baafb82235",
+    "url": "/react-gh-pages/static/js/main.40b33af7.chunk.js"
+  },
+  {
+    "revision": "e1966d8a1b6ce36f5314",
+    "url": "/react-gh-pages/static/js/runtime-main.2124a872.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
